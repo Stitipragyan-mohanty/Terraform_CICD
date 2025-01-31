11a2480e825837c29e07b8306f28494e854f2376
Stitipragyan-mohanty <stitipragyanm13@gmail.com>
2025-01-31 21:05:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "pubsub" {
   vpc_id                  = "vpc-071db49eccb9d4618"
   cidr_block              = "172.31.128.0/17"
   map_public_ip_on_launch = true
@@ -12,6 +12,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_instance" "dev" {
     ami = "ami-0c614dee691cbbf37"
     instance_type = "t2.micro"
+    subnet_id = aws_subnet.pubsub.id
     tags = {
       Name = "dev1234"
     }
