53f026c2f60f63630b7c44792d4576e75f689c24
Stitipragyan-mohanty <stitipragyanm13@gmail.com>
2025-01-31 20:26:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
     ami = "ami-00db8dadb36c9815e"
-    instance_type = "t2.nano"
+    instance_type = "t2.micro"
     tags = {
       Name = "dev123"
     }
