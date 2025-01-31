30426e628ab1c0d2d5b23b468ba3571b3e63264d
Stitipragyan-mohanty <stitipragyanm13@gmail.com>
2025-01-31 20:57:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "vpc-071db49eccb9d4618"
+  cidr_block              = "172.31.0.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
 
 resource "aws_instance" "dev" {
     ami = "ami-0c614dee691cbbf37"
