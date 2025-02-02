5da30b8234cb74744dcc834e5b921109eabd39f9
Stitipragyan-mohanty <stitipragyanm13@gmail.com>
2025-02-02 17:50:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "pubsub" {
   vpc_id                  = "vpc-0e5f5e7edfff2c818"
   cidr_block              = "10.0.168.0/21"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-1b"
+  availability_zone       = "us-west-2b"
 
   tags = {
     Name = "Public-Subnet"
